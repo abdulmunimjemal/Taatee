edfb8fef8d1df3a11b4a6415e4af3ac3bb1fc728
Abdulmunim <amunim729@gmail.com>
2024-01-07 00:04:04 +0300

    Added basic skeleton

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SigninDto, SignupDto } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+    
+    @Post('signup')
+    async signup(@Body() signupDto: SignupDto) {
+        return await this.authService.signup(signupDto);
+    }
+
+    @Post('signin')
+    async signin(@Body() signinDto: SigninDto) {
+        return await this.authService.signin(signinDto);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { SigninDto, SignupDto } from './dto';
 
+// we will use SQLite as our database
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        // private readonly usersService: UsersService,
+        // private readonly jwtService: JwtService,
+    ) {}
+
+    async signup(signupDto: SignupDto) {
+        // Logic to create user
+        
+    }
+
+    async signin(signinDto: SigninDto) {
+        // Logic to sign user in
+    }
+}
