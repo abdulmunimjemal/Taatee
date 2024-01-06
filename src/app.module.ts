bff350ba1c9fdb403090872a996379e0cdd7c6c6
Abdulmunim <amunim729@gmail.com>
2024-01-07 00:10:33 +0300

    Added Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { BookingModule } from './booking/booking.module';
 import { EventModule } from './event/event.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, EventModule, BookingModule],
+  imports: [AuthModule, UserModule, EventModule, BookingModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
