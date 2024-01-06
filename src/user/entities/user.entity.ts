415af6fdd243a63d9f82112a2ab2418e21c8e236
Abdulmunim <amunim729@gmail.com>
2024-01-07 00:10:21 +0300

    Added user entity

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 50 })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ default: false })
+    isAdmin: boolean;
+    
+}
