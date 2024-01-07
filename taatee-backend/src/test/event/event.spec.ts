f94ee491ae1d0f50dc39362f2846f7a7627b3f6d
salman <salmanalijijiga@gmail.com>
2024-01-08 00:16:06 +0400

    modified the imports

--- taatee-backend/src/test/event/event.spec.ts
+++ taatee-backend/src/test/event/event.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EventController } from './event.controller';
-import { EventService } from './event.service';
-import { Event } from './entities/event.entity';
+import { EventController } from '../event/event.controller';
+import { EventService } from '../event/event.service';
+import { Event } from '../event/entities';
+import { EventDto } from '../event/dto';
+import { BookingService } from '../booking/booking.service';
+import { Repository } from 'typeorm';
+import { UpdateEventDto } from '../event/dto';
