2a0edb06f7d46320613cba367808db2c14530db5
Abdulmunim <amunim729@gmail.com>
2024-01-08 01:59:26 +0300

    Registered Event Entity

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventController } from './event.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './entities';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Event]),
+  ],
   providers: [EventService],
   controllers: [EventController]
 })
