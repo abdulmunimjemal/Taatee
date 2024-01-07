02beafa02662aa6567b9544b6ba91a7049f6de7e
salman <salmanalijijiga@gmail.com>
2024-01-08 00:12:57 +0400

    pasted the necessary imports

---
+++ taatee-backend/src/test/event/event.spec.ts
@@ -0,0 +1,4 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EventController } from './event.controller';
+import { EventService } from './event.service';
+import { Event } from './entities/event.entity';
