fb701fea185c31ded042e8140063af65fa1f4bf9
Abdulmunim <amunim729@gmail.com>
2024-01-08 01:58:25 +0300

    Added Event Service

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
+import { Event } from './entities';
+import { EventDto } from './dto';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(
+    @InjectRepository(Event)
+    private readonly eventRepository: Repository<Event>,
+  ) {}
+
+  getAllEvents(): Promise<Event[]> {
+    return this.eventRepository.find();
+  }
+
+  async getEventById(id: number): Promise<Event> {
+    return await this.eventRepository.findOne({
+    where: {
+        id: id,
+    }
+    }); 
+  }
+
+  createEvent(event: EventDto): Promise<Event> {
+    return this.eventRepository.save(event);
+  }
+
+  updateEvent(id: number, updatedEvent: Event): Promise<Event> {
+    return this.eventRepository.save({ ...updatedEvent, id });
+  }
+
+  deleteEvent(id: number): Promise<void> {
+    return this.eventRepository.delete(id).then(() => undefined);
+  }
+}
