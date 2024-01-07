320eea734512352dac25b6b305d0529885d35076
Abdulmunim <amunim729@gmail.com>
2024-01-08 01:59:45 +0300

    Registered booking entity

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Booking } from './entities';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Booking]),
+  ],
   providers: [BookingService],
   controllers: [BookingController]
 })
