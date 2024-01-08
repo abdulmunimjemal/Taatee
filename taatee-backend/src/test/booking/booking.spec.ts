f9dc894387e8bd325fb9e88cb6b00175cfd51215
salman <salmanalijijiga@gmail.com>
2024-01-09 03:34:19 +0400

    written the base for the code

---
+++ taatee-backend/src/test/booking/booking.spec.ts
@@ -0,0 +1,7 @@
+jest.mock('../booking/booking.service');
+
+describe('-- Booking Controller Tests --', () => {
+    let bookingService: BookingService;
+    let module: TestingModule;
+    let bookingController: BookingController;
+});
