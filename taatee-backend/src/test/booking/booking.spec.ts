83361b0235ca931a5a24d6f65943ebb5191cdcfc
salman <salmanalijijiga@gmail.com>
2024-01-09 03:55:27 +0400

    boforeall code

--- taatee-backend/src/test/booking/booking.spec.ts
+++ taatee-backend/src/test/booking/booking.spec.ts
@@ -4,4 +4,15 @@ describe('-- Booking Controller Tests --', () => {
     let bookingService: BookingService;
     let module: TestingModule;
     let bookingController: BookingController;
+
+
+    beforeAll(async () => {
+        module = await Test.createTestingModule({
+          controllers: [BookingController],
+          providers: [BookingService],
+        }).compile();
+    
+        bookingService = module.get<BookingService>(BookingService);
+        bookingController = module.get<BookingController>(BookingController);
+      });
 });
